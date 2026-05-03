897866a7500a3a1b71c55866418d7c84124b4a6d
juneviii <dexterbot112@gmail.com>
2026-05-02 23:39:44 -0500

    Commit data

---
+++ data.py
@@ -0,0 +1,10 @@
+# URL for Urban Routes application
+URBAN_ROUTES_URL = 'https://cnt-6f0cd3f2-e65f-4982-a77d-ac5a6f7ed3ce.containerhub.tripleten-services.com/'
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
